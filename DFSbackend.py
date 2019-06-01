62086a175b94f2acc9e68058780c2e570f41b9d3
Brian Neville <briannev2010@gmail.com>
2019-06-01 01:15:16 +0100

    - working on the message parsing

--- DFSbackend.py
+++ DFSbackend.py
@@ -22,6 +22,8 @@ def parsedcommand(func):
     return wrapping_test
 '''
 
+HOME_DIR_NAME = '_/'
+
 
 def pickle_obj(fname, graph) ->None:
     # saves the client graph that is being used for the filesystem
@@ -45,17 +47,27 @@ async def tcp_echo_client(send_msg, event_loop) ->None:
     print("data= " + data.decode())
     writer.close()
 
+open_dir_args = ['full_path','back_dirpath','home']
+
 
 def parsedcommand(func):
     def wrapper(*args, **kwargs):
-        if kwargs['use_open_dir'] == -1:
-            initialised = func(*args, kwargs['full_path'])
+        if kwargs['open_dir_choice'] >= 0:
+            chosen_arg = open_dir_args[kwargs['open_dir_choice']]
+            print(chosen_arg)
+            initialised = func(*args, kwargs[chosen_arg])
+
         else:
             initialised = func(*args, kwargs['current_dirpath'], kwargs['full_path'])
         return initialised
 
     return wrapper
 
+
+def get_backpath(command) ->str:
+    command = command[:-1:]
+    end = command.rfind('/')
+    return command[:end + 1:]
 
 
 class DFShandler:
@@ -64,7 +76,7 @@ class DFShandler:
         self.servers_available = None   # the amount of servers avaiable for the client to distribute files on
         self.uid = uid
         self.graph = {
-            './': []         # home directory : ["path/folder_x", "path/folder_y" "path/file.txt" etc ]
+            HOME_DIR_NAME: []         # home directory : ["path/folder_x", "path/folder_y" "path/file.txt" etc ]
             #                 format=  dir: [list of files and directories at this location]
         }
         fname = "graph_"+str(uid)
@@ -73,13 +85,14 @@ class DFShandler:
         except FileNotFoundError:
             pickle_obj(fname, self.graph)
 
-        self.parseoptions = {
+        self.parseoptions = {       # 'key':
             'cd': self.open_dir,
             'home': self.open_dir,
             'back': self.open_dir,
             'get': self.get_file,
-            'up': self.upload_file
-
+            'up': self.upload_file,
+            'fnew': self.add_to_folder,
+            'fdel': self.remove_from_folder
         }
 
     def divide_into_chunks(self, filepath) ->(str, list):
@@ -129,16 +142,20 @@ class DFShandler:
 
     @parsedcommand
     def open_dir(self, new_dir_path):
-        return self.graph[new_dir_path] # dir_path has values eg: "./" or "./folder1/folder2/" or "./diffolder1/"
+        print(f'new dir to open={new_dir_path}')
+
+        # return self.graph[new_dir_path] # dir_path has values eg: "./" or "./folder1/folder2/" or "./diffolder1/"
 
     @parsedcommand
     def get_file(self, file_path):
         # retrieve file from servers online
-        print(f"file patth recieved!! {file_path}")
-        pass
+        print(f"file to get: {file_path}")
 
     @parsedcommand
     def add_to_folder(self, dirpath, filepath):     # dir path of parent folder
+        # add an item to a parent folder
+        print(f"parent folder {dirpath}, new file name = {filepath}")
+        pass
         curr = self.graph[dirpath]
         curr.append(filepath)
         self.graph[dirpath] = curr
@@ -146,11 +163,13 @@ class DFShandler:
 
     @parsedcommand
     def remove_from_folder(self, dirpath, filepath):
+        # remove an item from a parent folder
+        print(f"parent folder {dirpath}, file to remove name = {filepath}")
+        pass
         curr = self.graph[dirpath]
         curr.remove(filepath)
         self.graph[dirpath] = curr
         # TODO: send delete message to servers
-
 
     def parse(self, msg, current_dirpath):
         # parses an item from the queue
@@ -161,29 +180,55 @@ class DFShandler:
         # back - return to the parent directory unless at home
         # get - downlaod a file from a directory
         # up - upload a file from your pc to the servers
+        # fnew - create a new folder
+        # fdel - delete a folder
         full_path = None    # full path is the entire path for the file or dir to be used for indexing the graph dict
         space_pos = msg.find(' ')
         subject = msg[space_pos+1:] if space_pos >= 0 else None     # ex subfolder1
         command = msg[:space_pos] if space_pos >= 0 else msg        # ex cd
-        if command == 'cd':
-            space_pos = -1      # use this to signify that we just want to open a directory and not change any files
         if msg[-1:] == '/' or (subject is not None and subject.find('.') >= 0):
             full_path = current_dirpath + subject
 
-        self.parseoptions[command](use_open_dir=space_pos, current_dirpath=current_dirpath, full_path=full_path)
+        # when using open dir commands, 0 is open sub dir, 1 is back, 2 is home
+        use_cur_dir = msg.find('cd')
+        use_cur_dir = 1 if not msg.find('back') else use_cur_dir    # 'if not find' will be true if back is at the start
+        use_cur_dir = 2 if not msg.find('home') else use_cur_dir        # of the msg
+        # default value for none of these commads found is -1
 
-        print(space_pos, command, subject, full_path)
+        self.parseoptions[command](
+            open_dir_choice=use_cur_dir,
+            current_dirpath=current_dirpath,
+            home=HOME_DIR_NAME,
+            back_dirpath=None if command != 'back' else get_backpath(current_dirpath),
+            full_path=full_path
+        )
+        # yeah i know this might not be incredibly efficient/perfect but i just wanted to use decorators
 
 
 dfs = DFShandler(uid=1234)
 dfs.servers_available = 3
 # dfs.divide_into_chunks('D:/Users/Brian/PycharmProjects/dfs/undivided.txt')
-dfs.parse('cd sub1/', './parent/')
+# dfs.parse('cd sub2/', '_/parent/sub1/')
+# dfs.parse('home', '_/parent/sub1/')
+# dfs.parse('back', '_/parent/sub1/')
+# dfs.parse('get filena.dat', './parent/sub1/')
+# dfs.parse('up c:/users/brn/filex.txt', './parent/sub1/')
 
 # dfs.get_file(file_path='./f1/f2/', testing= 1)
 # dump_client_graph( str(dfs.uid),graph0)
 # graph2 = load_client_graph(str(dfs.uid))
 # print(graph2)
+
+
+
+
+
+
+
+
+
+
+
 
 # TODO: things to do for distributed file system:
 # 1. generate secure private key to encyrpt user data
@@ -194,24 +239,6 @@ dfs.parse('cd sub1/', './parent/')
 #       - keep a dictionary of {chunk_id, [(uid_1002), (uid_2321), (uid_78) ... (uid_34) ]}
 #       - keep another dictionary mapping each of these uid numbers to IP and PORT numbers
 #       - first try downloaded from uid_1002; if no connection to user, then the user offline; retry with uid_2332, etc
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
